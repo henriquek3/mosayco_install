e4d27c22f60d9e562fbc30835b311ca9814957b9
Jean Freitas <henriquek3@live.com>
2017-05-05 17:32:43 -0400

    init

---
+++ main.cpp
@@ -0,0 +1,9 @@
+
+#include <afxres.h>
+#include "menuPrincipal.h"
+
+int main() {
+    SetConsoleOutputCP(65001);
+    menuPrincipal().menuOpcaoPrincipal();
+    return 0;
+}
--- menuPrincipal.h
+++ menuPrincipal.h
@@ -20,7 +20,7 @@ public:
         while (1 == 1) {
             system("cls");
             system("color A");
-            menuOpcoes();
+            menuOpcaoPrincipal();
             system("pause");
             exit(0);
         }
@@ -57,6 +57,96 @@ public:
                 "|===============================|" << endl;
     }
 
+    void menuOpcaoPrincipal() {
+        cout <<
+             "|===============================================================================================|\n"
+                     "|\t\t\t DIGITE O NÚMERO DE UMA OPCÃO PARA INICIAR\t\t\t\t|\n"
+                     "|===============================================================================================|\n"
+                     "|\t\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 01) FINANCEIRO\t| 05) COMPRAS\t\t| 09) PULVERIZA\t\t| 13) TRANSMISSAO\t|\n"
+                     "| 02) ALMOXAR\t\t| 06) FISCAL\t\t| 10) SPED\t\t| 14) SGL\t\t|\n"
+                     "| 03) SEMENTES\t\t| 07) ALGODAO\t\t| 11) NFE\t\t| 15) TRANSMITE\t\t|\n"
+                     "| 04) GRAOS\t\t| 08) PATRIMONIO\t| 12) SIGA\t\t| 16) RECEBE\t\t|\n"
+                     "|\t\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "|===============================================================================================|\n"
+                     "| A) INSTALAR MOSAYCO\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| B) OPCOES WINDOWS\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| C) OPCOES ORACLE\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| F) CENTRAL DE TESTES\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| X) SAIR\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "|===============================================================================================|"
+             <<
+             endl;
+    }
+
+    void menuOpcao_A() {
+        cout <<
+             "|===============================================================================================|\n"
+                     "| A) INSTALAR MOSAYCO\t\t\t\t\t\t\t\t\t\t|\n"
+                     "|===============================================================================================|\n"
+                     "|\t\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 1) CRIAR DIRETÓRIOS - SISTEMA\t\t\t\t\t\t\t\t\t|\n"
+                     "| 2) CRIAR DIRETÓRIOS - USUARIOS\t\t\t\t\t\t\t\t|\n"
+                     "| 3) BAIXAR INSTALADORES\t\t\t\t\t\t\t\t\t|\n"
+                     "| 4) EXECUTAR INSTALADORES\t\t\t\t\t\t\t\t\t|\n"
+                     "| 5) ADICIONAR PERMISSOES\t\t\t\t\t\t\t\t\t|\n"
+                     "| 6) CONFIGURAR ODBC\t\t\t\t\t\t\t\t\t\t|\n"
+                     "|\t\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "|===============================================================================================|\n"
+                     "| V ) VOLTAR\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| M ) MENU PRINCIPAL\t\t\t\t\t\t\t\t\t\t|\n"
+                     "|===============================================================================================|"
+             <<
+             endl;
+    }
+
+    void menuOpcao_A_3() {
+        cout <<
+             "|===============================================================================================|\n"
+                     "| A) -> 3) BAIXAR INSTALADORES\t\t\t\t\t\t\t\t\t|\n"
+                     "|===============================================================================================|\n"
+                     "|\t\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 1) ORACLEXE\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 2) BDE 32 BITS\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 3) BDE 64 BITS\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 4) DLLS\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 5) DOSPRINT\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 6) ATUALIZADOR BATCH\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 7) CUTEPDF\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 8) PLSQL\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 9) APP ORA28000\t\t\t\t\t\t\t\t\t\t|\n"
+                     "|\t\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "|===============================================================================================|\n"
+                     "| V ) VOLTAR\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| M ) MENU PRINCIPAL\t\t\t\t\t\t\t\t\t\t|\n"
+                     "|===============================================================================================|"
+             <<
+             endl;
+    }
+
+    void menuOpcao_A_4() {
+        cout <<
+             "|===============================================================================================|\n"
+                     "| A) -> 4) EXECUTAR INSTALADORES\t\t\t\t\t\t\t\t|\n"
+                     "|===============================================================================================|\n"
+                     "|\t\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 1) ORACLEXE\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 2) BDE 32 BITS\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 3) BDE 64 BITS\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 4) DLLS\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 5) DOSPRINT\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 6) ATUALIZADOR BATCH\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 7) CUTEPDF\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 8) PLSQL\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| 9) APP ORA28000\t\t\t\t\t\t\t\t\t\t|\n"
+                     "|\t\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "|===============================================================================================|\n"
+                     "| V ) VOLTAR\t\t\t\t\t\t\t\t\t\t\t|\n"
+                     "| M ) MENU PRINCIPAL\t\t\t\t\t\t\t\t\t\t|\n"
+                     "|===============================================================================================|"
+             <<
+             endl;
+    }
 };
 
 
