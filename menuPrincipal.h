f0d24a007e1b1ea01d89e899c97a20e534205aa2
Jean Freitas <henriquek3@live.com>
2017-05-06 10:52:17 -0400

    arrumando funções

--- menuPrincipal.h
+++ menuPrincipal.h
@@ -14,7 +14,7 @@ class menuPrincipal {
 
 private:
     int show = 1;
-    int opcao;
+    string opcao;
     int modulo;
     string instalar = "A";
     string windows = "B";
@@ -28,7 +28,8 @@ public:
             system("cls");
             menuOpcaoPrincipal();
             cin >> opcao;
-            verificaModulos(opcao);
+            verificaModulos(retornaNumeroOpcao(opcao));
+            system("pause");
         }
     }
 
@@ -168,13 +169,60 @@ public:
             case 2:
                 cout << "Verificação 02" << endl;
                 break;
-            case 3:
-                cout << "Verificação 03" << endl;
+            case 99:
+                cout << "Verificação 99" << endl;
                 break;
             default:
                 cout << "Digite uma opcao valida!" << endl;
                 system("pause");
         }
+    }
+
+    int retornaNumeroOpcao(string arg) {
+        if (arg == "01" || arg == "1") {
+            return 1;
+        } else if (arg == "02" || arg == "2") {
+            return 2;
+        } else if (arg == "03" || arg == "3") {
+            return 3;
+        } else if (arg == "04" || arg == "4") {
+            return 4;
+        } else if (arg == "05" || arg == "5") {
+            return 5;
+        } else if (arg == "06" || arg == "6") {
+            return 6;
+        } else if (arg == "07" || arg == "7") {
+            return 7;
+        } else if (arg == "08" || arg == "8") {
+            return 8;
+        } else if (arg == "09" || arg == "9") {
+            return 9;
+        } else if (arg == "10") {
+            return 10;
+        } else if (arg == "11") {
+            return 11;
+        } else if (arg == "12") {
+            return 12;
+        } else if (arg == "13") {
+            return 13;
+        } else if (arg == "14") {
+            return 14;
+        } else if (arg == "15") {
+            return 15;
+        } else if (arg == "16") {
+            return 16;
+        } else if (arg == "A" || arg == "a") {
+            return 17;
+        } else if (arg == "B" || arg == "b") {
+            return 18;
+        } else if (arg == "C" || arg == "c") {
+            return 19;
+        } else if (arg == "D" || arg == "d") {
+            return 20;
+        } else if (arg == "X" || arg == "x") {
+            return 21;
+        }
+        return 99;
     }
 };
 
---
+++ testes.h
@@ -0,0 +1,17 @@
+//
+// Created by henri on 06/05/2017.
+//
+
+#ifndef MOSAYCO_INSTALL_TESTES_H
+#define MOSAYCO_INSTALL_TESTES_H
+
+#include <iostream>
+#include <cstdlib>
+
+class testes {
+    void retorno(char args) {
+
+    }
+};
+
+#endif //MOSAYCO_INSTALL_TESTES_H
